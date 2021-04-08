179a7dc3ac2c34106274d049680b1d2d56ee81e7
gima <gimantha.k98@gmail.com>
2021-04-08 13:56:45 +0530

    deploy to netlify

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Gima's Todo List</h1>
+        <h1>Gima's Todo List - Netlify</h1>
       </header>
       <Form
         inputText={inputText}
